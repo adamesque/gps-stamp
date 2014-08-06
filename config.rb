f8b275c2e81fd059978d341754d04c4b38b35ec4
Adam Luikart <adam@reactionhousing.com>
2014-08-06 00:20:34 -0500

    Deploy to GitHub Pages

--- config.rb
+++ config.rb
@@ -28,6 +28,15 @@
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
 
+activate :deploy do |deploy|
+  deploy.method = :git
+  # Optional Settings
+  # deploy.remote   = "custom-remote" # remote name or git url, default: origin
+  # deploy.branch   = "custom-branch" # default: gh-pages
+  # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
+  # deploy.commit_message = "custom-message"      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
+end
+
 ###
 # Helpers
 ###
